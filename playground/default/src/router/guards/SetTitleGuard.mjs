5e80d6b00d1048ea63dfecfa6ce513cccd2d3059
Ignácz Dominik Bence <dominikignacz90@gmail.com>
2023-06-26 12:31:56 +0200

    version 23.6.26-5 snapshot

--- playground/default/src/router/guards/SetTitleGuard.mjs
+++ playground/default/src/router/guards/SetTitleGuard.mjs
@@ -1,6 +1,6 @@
 import {useHead} from "@vueuse/head";
 
 export function setTitle(to, from, next) {
-  useHead({title: to.meta.title + process.env.npm_package_name});
+  useHead({title: to.meta.title});
   next()
 }
