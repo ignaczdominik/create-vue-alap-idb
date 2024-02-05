36317cf2c8a44a3d36f5f5e44f3e685235585249
Ignácz Dominik Bence <dominikignacz90@gmail.com>
2024-02-05 10:52:24 +0100

    version 1.24.2-5-6 snapshot

--- playground/default/src/main.js
+++ playground/default/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from '@/router/index.js'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { plugin, defaultConfig } from '@formkit/vue'
 
 import App from '@/App.vue'
 
@@ -14,6 +15,7 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 app.use(router)
+app.use(plugin, defaultConfig)
 app.use(pinia)
 
 app.mount('#app')
