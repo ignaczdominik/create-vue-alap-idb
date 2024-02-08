d33fd81846822a240339f484080ac7858977dcb1
Ignácz Dominik Bence <dominikignacz90@gmail.com>
2024-02-08 08:31:48 +0100

    version 1.24.2-8-3 snapshot

--- playground/default/src/pages/HomePage.vue
+++ playground/default/src/pages/HomePage.vue
@@ -1,15 +1,19 @@
-<script setup>
-import BaseLayout from '@layouts/BaseLayout.vue'
-import { useCounter } from '@stores/CounterStore.mjs'
-
-const CounterStore = useCounter()
-</script>
-
 <template>
   <BaseLayout class="text-center">
     <h1 class="display-1 my-5">Hello!</h1>
-    <button class="btn btn-success" @click="CounterStore.increment()">
-      Számláló: {{ CounterStore.counter }}
+    <button class="btn btn-success" @click="useCounter().increment()">
+      Számláló: {{ useCounter().counter }}
     </button>
   </BaseLayout>
 </template>
+
+<script>
+import BaseLayout from '@layouts/BaseLayout.vue'
+import { useCounter } from '@stores/CounterStore.mjs'
+
+export default {
+  components: {
+    BaseLayout
+  }
+}
+</script>
